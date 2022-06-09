1280c7d910baed111dc8358d62a7523493889373
Aleks-NT <mr.very@yandex.ru>
2022-06-10 00:22:04 +0500

    Initial commit

---
+++ src/main/java/com/example/SpringBoot/model/User.java
@@ -0,0 +1,21 @@
+package com.example.SpringBoot.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "age")
+    private int age;
+    @Column(name = "email")
+    private String email;
+}
